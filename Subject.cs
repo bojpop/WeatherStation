010f573a3fae3b6450e43e9649d93e7faa11d732
bojpop <poppfc@gmail.com>
2016-06-06 23:38:07 +0200

    First commit

---
+++ Subject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    interface Subject
+    {
+        void registerObserver(Observer o);
+        void removeObserver(Observer o);
+        void notifyObservers();
+    }
+}
